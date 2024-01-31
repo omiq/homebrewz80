d496cdb69aae6891e09124734af66f72cdd9932f
Chris Garrett <chris@chrisg.com>
2024-01-31 20:24:50 +0000

    Create clock.ino

---
+++ clock.ino
@@ -0,0 +1,98 @@
+#define AUTO  A0
+#define CLOCKLED 5
+#define BUTTON_PIN 7
+
+// Initialize the speed to OFF
+unsigned int speed = 0;
+unsigned int oldSpeed = 0;
+unsigned int steps = 0;
+
+void sleep(int sleep_time)
+{
+  long start_time = millis();
+
+  while(millis() < start_time + sleep_time)
+  {
+    // do nothing
+  }
+}
+
+// Tick the clock
+void toggle_clock()
+{
+  digitalWrite(CLOCKLED, HIGH);
+  sleep(10);
+  digitalWrite(CLOCKLED, LOW);
+  sleep(10);
+}
+
+// Single increment of the clock
+bool single_step()
+{
+  toggle_clock();
+  Serial.print("STEP ");
+  Serial.println(steps);
+  steps++;
+  sleep(500);
+}
+
+
+void setup() {
+
+  // set up the pins and the event listeners
+  pinMode(CLOCKLED, OUTPUT);  
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(AUTO, INPUT);
+
+  // initialize serial communication 
+  Serial.begin(115200);
+
+  // Output the status of the buttons/switches
+  Serial.print("POT: ");
+  Serial.println(analogRead(AUTO));
+
+  Serial.print("STEP: ");
+  Serial.println(digitalRead(BUTTON_PIN));
+
+}
+
+void loop()
+{
+
+  // What is the pot set to?
+  speed = analogRead(AUTO);
+  
+  if(speed != oldSpeed) {
+
+    oldSpeed = speed;
+
+    if(speed == 1023) 
+    {
+      // Manual stepping listener
+      Serial.println("AUTO OFF");
+
+    }
+    else
+    {
+      // Set the speed to the trim pot
+      Serial.print("POT: ");
+      Serial.println(speed);
+    }
+
+  }
+  else
+  {
+      sleep(10);
+
+      if(speed == 1023) {
+        if(digitalRead(BUTTON_PIN))
+        {
+          single_step();
+        }
+      } else {
+        sleep(speed);
+        toggle_clock();
+      }
+  } 
+}
+
