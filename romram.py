298924f636a27dfcab4ace97397ed5f4cc83cbcd
Chris Garrett <chris@chrisg.com>
2024-02-04 15:17:03 +0000

    Update romram.py

--- romram.py
+++ romram.py
@@ -15,25 +15,19 @@ def set_pins(pins, data):
             pin.off()
         data = (data >> 1)
 
-def m_pin_handler(p):
-    if MREQ.value():
-        led_pin.value(0)
-    else:
-        led_pin.value(1)
-    sleep(0.1) # wait for 0.1 second
-    
+  
 def io_pin_handler(p):
     if IOREQ.value()==False:
         print(top + down + "IO: ON")
     sleep(0.1) # wait for 0.1 second
 
 def w_pin_handler(p):
-    if MREQ.value()==False:
+    if WR.value()==False:
         print(top + down + "W: ON")
     sleep(0.1) # wait for 0.1 second
 
 def r_pin_handler(p):
-    if MREQ.value()==False:
+    if RD.value()==False:
         print(top + down + "R: ON")
     sleep(0.1) # wait for 0.1 second
 
@@ -41,23 +35,23 @@ def r_pin_handler(p):
 tick = 0
 
 # ROM/RAM
-memory = []
-for b in range(65536):
-    memory.append(0)
+ram_memory = []
+for b in range(16384):
+    ram_memory.append(0)
 
 # LED status light
 led_pin = Pin("LED", Pin.OUT)
 
 # Write and Read signals
 WR = Pin("GP0", Pin.IN)
-WR.irq(handler=w_pin_handler,trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
+#WR.irq(handler=w_pin_handler,trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
 RD = Pin("GP1", Pin.IN)
-RD.irq(handler=r_pin_handler,trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
+#RD.irq(handler=r_pin_handler,trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
 # IO Request
 IOREQ = Pin("GP27", Pin.IN, Pin.PULL_UP)
-IOREQ.irq(handler=io_pin_handler,trigger=Pin.IRQ_RISING)
+#IOREQ.irq(handler=io_pin_handler,trigger=Pin.IRQ_RISING)
 
 # Memory Request
 # MREQ = Pin("GP28", Pin.IN)
@@ -75,7 +69,6 @@ datavalues = []
 data_pins = [16,17,18,19,20,21,22,26]
 for pin_number in data_pins:
     datavalues.append(Pin(pin_number, Pin.OUT))
-    datavalues[pin_number].value(0) # Set to NOP
 
 # Blink
 print(clear)
@@ -122,8 +115,8 @@ def romram(pin):
         
         for data_pin in reversed(datavalues):
             data_pin.init(mode=Pin.OUT)
-        set_pins(datavalues, memory[this_address])
-        this_data = memory[this_address]
+        set_pins(datavalues, ram_memory[this_address])
+        this_data = ram_memory[this_address]
 
     # Assuming is writing    
     else:
@@ -131,16 +124,17 @@ def romram(pin):
             data_pin.init(mode=Pin.IN)
         for data_pin in reversed(datavalues):
             this_data = (this_data << 1) + data_pin.value()
-            memory[this_address] = this_data
+            ram_memory[this_address] = this_data
 
     print(top)         
-    print(tick, "RD: ", is_reading, "Address:", '0x{:04x}'.format(this_address), "Data:", '0x{:04x}'.format(datavalues),end='')
+    print('{:8}'.format(tick), end='') 
+    print(bold + " IO: " + reset  + str(IOREQ.value()),end='')
+    print(bold + " RD: " + reset, is_reading, end='')
+    print(bold + " WR: " + reset  + str(WR.value()), end='')
+    print(bold + " Address: " + reset, '0x{:04x}'.format(this_address), end='')
+    print(bold + " Data: " + reset, bin(this_data))
     
-    print(bold + " IO:" + reset  + str(IOREQ.value()),end='')
-    print(bold + " WR:" + reset  + str(WR.value()),end='')
-
-    
-    print(address)
+    print()  
     print(clearline)
     
     tick = tick + 1
@@ -148,8 +142,4 @@ def romram(pin):
 
 # Clock
 Clock = Pin("GP28", Pin.IN)
-Clock.irq(handler=romram,trigger=Pin.IRQ_RISING)
-
-  
-
-
+Clock.irq(handler=romram, trigger=Pin.IRQ_RISING)
